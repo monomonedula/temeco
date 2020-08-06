7e711f30a715beeba6029cfb58ee201cbc9effb5
monomonedula <valh@tuta.io>
2020-08-06 12:31:56 +0300

    docs

--- temeco/interface.py
+++ temeco/interface.py
@@ -2,24 +2,50 @@ from abc import ABC, abstractmethod
 
 
 class Entity(ABC):  # pragma: no cover
+    """
+    An interface representing a Telegram message entity.
+    """
     @abstractmethod
     def offset(self) -> int:
+        """
+        :return: entity offset (int)
+        """
         pass
 
     @abstractmethod
     def length(self) -> int:
+        """
+        :return: entity length (int)
+        """
         pass
 
     @abstractmethod
     def as_html_str(self) -> str:
+        """
+        :return: an HTML version of the entity offset (str)
+        """
         pass
 
 
 class EncodingAwareText(ABC):   # pragma: no cover
+    """
+    An interface representing Telegram message with utf-16 encoding awareness
+    """
     @abstractmethod
     def text_of(self, start: int, stop: int = None) -> str:
+        """
+        start - start index
+        stop - stop index.
+        Only positive indices supported.
+        :return: slice the text with the given indices.
+        If stop=None then then the slice ends at the end of the string
+        """
         pass
 
     @abstractmethod
     def entity_text(self, entity: Entity) -> str:
+        """
+        A shortcut for text_of method.
+        :return: an HTML version of the entity offset (str)
+        """
         pass
--- temeco/temeco.py
+++ temeco/temeco.py
@@ -5,6 +5,15 @@ from temeco.interface import Entity, EncodingAwareText
 
 
 class BasicEntity(Entity):
+    """
+    A class representing a Telegram message entity.
+    Supporting the following entity types:
+     - `bold`
+     - `italic`
+     - `text_link`
+     - `code` (monospace text)
+     - `pre` (preformatted text)
+    """
     def __init__(
         self,
         msg_text: "EncodingAwareText",
@@ -20,12 +29,22 @@ class BasicEntity(Entity):
         self._data = data or {}
 
     def offset(self) -> int:
+        """
+        :return: entity offset (int)
+        """
         return self._offset
 
     def length(self) -> int:
+        """
+        :return: entity length (int)
+        """
         return self._len
 
     def as_html_str(self,) -> str:
+        """
+        :return: an HTML version of the entity offset (str)
+        If the entity type isn't supported, the entity text is returned untouched.
+        """
         if self._type == "bold":
             return f"<b>{self._msg.entity_text(self)}</b>"
         elif self._type == "italic":
@@ -40,24 +59,44 @@ class BasicEntity(Entity):
 
 
 class TelegramUTF16Text(EncodingAwareText):
-    def __init__(self, txt):
+    """
+    A class representing Telegram message with utf-16 encoding awareness
+    """
+    def __init__(self, txt: str):
         self._utf16_txt = txt.encode("utf-16-le")
 
     def text_of(self, start: int, stop: int = None):
+        """
+        start - start index
+        stop - stop index.
+        Only positive indices supported.
+        :return: slice the text with the given indices.
+        If stop=None then then the slice ends at the end of the string
+        """
         if stop is None:
             stop = len(self._utf16_txt) // 2
         return self._utf16_txt[start * 2 : stop * 2].decode("utf-16-le")
 
     def entity_text(self, entity: Entity):
+        """
+        A shortcut for text_of method.
+        :return: an HTML version of the entity offset (str)
+        """
         return self.text_of(entity.offset(), entity.offset() + entity.length())
 
 
 class HtmlFromMsg:
+    """
+    A class for entities to HTML translation
+    """
     def __init__(self, msg_txt: "EncodingAwareText", entities: List[Entity]):
         self._txt = msg_txt
         self._entities = entities
 
     def as_str(self) -> str:
+        """
+        :return: HTML version of the original message
+        """
         subs = []
         for e in self._entities:
             subs.append(e.as_html_str())
